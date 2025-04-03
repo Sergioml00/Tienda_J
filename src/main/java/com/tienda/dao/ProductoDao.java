e74c06df6f05bd578139eb3863eab781b910ab11
Sergioml00 <smata20256@ufide.ac.cr>
2025-04-02 23:08:34 -0600

    Clase 8
    Commit de la clase de reposición, video que mando la profe

--- src/main/java/com/tienda/dao/ProductoDao.java
+++ src/main/java/com/tienda/dao/ProductoDao.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 
 package com.tienda.dao;
 
 import com.tienda.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-/**
- *
- * @author sergi
- */
-public interface ProductoDao extends JpaRepository <Producto, Long> {
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+public interface ProductoDao extends JpaRepository <Producto, Long>{
+ public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
 }
